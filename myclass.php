be799f884205a5834a9633b2a091e9eaa8348238
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-10-09 14:57:48 +0100

    great

--- myclass.php
+++ myclass.php
@@ -12,13 +12,13 @@ class Profile{
   global $db;
   $pass = password_hash($password, PASSWORD_BCRYPT);
   $sql = $db->query("INSERT INTO users(name, email, phone, password) VALUES('$name', '$email', '$phone', '$pass')");
-  if($sql){"operation successful";}
+  if($sql){echo "operation successful";}
  }
 }
 
 $pro = new Profile();
 if(isset($_GET['type'])){extract($_GET);
- if($type=='signup'){$pro->SignUp($name,$email,$phone,$passwrd);
+ if($type=='signup'){$pro->SignUp($name,$email,$phone,$pass);
  }
 }
 
