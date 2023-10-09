e326bf79ea5169a7949b0cfb49270bef3bf6a750
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-10-09 14:42:17 +0100

    great

--- myclass.php
+++ myclass.php
@@ -4,13 +4,21 @@ $db = new mysqli('localhost', 'root', '', 'chatapp');
 
 class Profile{
  function __construct(){
+  
 
  }
 
  function SignUp($name, $email, $phone, $password){
   global $db;
   $pass = password_hash($password, PASSWORD_BCRYPT);
-  $sql = $db->query("INSERT INTO users(name, email, phone, password) VALUES('$name', '$email', '$phone', '$password')");
+  $sql = $db->query("INSERT INTO users(name, email, phone, password) VALUES('$name', '$email', '$phone', '$pass')");
+  if($sql){"operation successful";}
+ }
+}
+
+$pro = new Profile();
+if(isset($_GET['type'])){extract($_GET);
+ if($type=='signup'){$pro->SignUp($name,$email,$phone,$passwrd);
  }
 }
 
