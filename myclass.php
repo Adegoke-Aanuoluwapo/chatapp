d567b418a4d053ef07350711fb7bb344b1ceafca
Adegoke-Aanuoluwapo <Aanummaculate@gmail.com>
2023-10-10 11:22:39 +0100

    more

--- myclass.php
+++ myclass.php
@@ -11,7 +11,7 @@ class Profile{
  function SignUp($name, $email, $phone, $password){
   global $db;
   $pass = password_hash($password, PASSWORD_BCRYPT);
-  $sql = $db->query("SELECT * FROM users WHERE email = '$email' AND phone ='$phone'  ");
+  $sql = $db->query("SELECT * FROM users WHERE email = '$email' OR phone ='$phone'  ");
   if(mysqli_num_rows($sql) > 0){
    echo "email or phone already exist";
   }else{
@@ -22,11 +22,29 @@ class Profile{
   };
   
  }
+
+ function LoginUser($email, $password){
+  global $db;
+  $sql= $db->query("SELECT * FROM user WHERE email ='$email' ");
+  if(mysqli_num_rows($sql) != 1){
+   echo "invalid login";
+   return;
+  }
+  $rows = mysqli_fetch_assoc($sql);
+  if(password_verify($password, $rows['password'])){
+   $_SESSION['id'] = $rows['id'];
+   header('location:chat.html');
+   echo 'login success';
+   return;
+  };
+  echo 'invalid login';
+  return;
+ }
 };
 
 $pro = new Profile();
 if(isset($_GET['type'])){extract($_GET);
- if($type=='signup'){$pro->SignUp($name, $email, $phone, $pass);
+ if($type=='signup'){$pro->SignUp($name, $email, $phone, $password);
  }
 }
 
